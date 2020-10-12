6a74f0960e0f2f4fe2c6b1b8e09060a8d10936a1
tutur1004 <arthur.barthelme78@gmail.com>
2020-10-12 02:45:16 +0200

    - Fusion commande rg-reload - Ajout fonction agence - LinkedHashMap pour les regions pour garder l'ordre

--- src/main/java/fr/milekat/cite_claim/MainClaim.java
+++ src/main/java/fr/milekat/cite_claim/MainClaim.java
@@ -1,13 +1,14 @@
 package fr.milekat.cite_claim;
 
 import fr.milekat.cite_claim.commands.CommandsRegion;
-import fr.milekat.cite_claim.commands.ForceReloadRegions;
 import fr.milekat.cite_claim.commands.ModeBuilder;
 import fr.milekat.cite_claim.commands.TabsRegion;
+import fr.milekat.cite_claim.engines.AgenceUpdate;
 import fr.milekat.cite_claim.engines.RegionsTask;
 import fr.milekat.cite_claim.engines.SpeedBoost;
 import fr.milekat.cite_claim.events.BoundClaim;
 import fr.milekat.cite_claim.events.EventsCite;
+import fr.milekat.cite_claim.events.OpenAgence;
 import fr.milekat.cite_claim.events.RegionMarket;
 import fr.milekat.cite_claim.obj.Region;
 import fr.milekat.cite_claim.utils.RegionsBlocksLoad;
@@ -15,38 +16,44 @@ import fr.milekat.cite_core.MainCore;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.UUID;
 
 public class MainClaim extends JavaPlugin implements Listener {
     // Init des var static, pour tous le projet
     public static String prefixConsole = "[Balkoura-claim] ";
-    public static HashMap<String, Region> regions = new HashMap<>();
+    public static LinkedHashMap<String, Region> regions = new LinkedHashMap<>();
     public static HashMap<Location, String> regionsBlocks = new HashMap<>();
     public static HashMap<UUID, Location> boundRegionright = new HashMap<>();
     public static HashMap<UUID, Location> boundRegionleft = new HashMap<>();
+    public static ItemStack bookAgence;
     private static MainClaim mainClaim;
     private BukkitTask regionsEngine;
     private BukkitTask speedEngine;
+    private BukkitTask agenceEngine;
 
     @Override
     public void onEnable() {
         mainClaim = this;
         // Reload de la map des blocks
         new RegionsBlocksLoad().reloadRegions();
+        new RegionsTask().updateRegion();
         regionsEngine = new RegionsTask().runTask();
         speedEngine = new SpeedBoost().runTask();
+        agenceEngine = new AgenceUpdate().runTask();
         // Events
         getServer().getPluginManager().registerEvents(new BoundClaim(),this);
         //getServer().getPluginManager().registerEvents(new PlaceEvent(),this);
         getServer().getPluginManager().registerEvents(new EventsCite(),this);
         getServer().getPluginManager().registerEvents(new RegionMarket(),this);
+        getServer().getPluginManager().registerEvents(new OpenAgence(),this);
         // Commandes
         getCommand("build").setExecutor(new ModeBuilder());
-        getCommand("rg-reload").setExecutor(new ForceReloadRegions());
         getCommand("region").setExecutor(new CommandsRegion());
         // Tabs
         getCommand("region").setTabCompleter(new TabsRegion());
@@ -56,6 +63,7 @@ public class MainClaim extends JavaPlugin implements Listener {
     public void onDisable(){
         regionsEngine.cancel();
         speedEngine.cancel();
+        agenceEngine.cancel();
     }
 
     public static MainClaim getInstance(){
